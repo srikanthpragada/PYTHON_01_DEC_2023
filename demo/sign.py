e89ad84aada105be9e6b40bc43c9ae778e22ab2e
srikanthpragada <srikanthpragada@gmail.com>
2023-12-05 19:50:26 +0530

    05-DEC

---
+++ demo/sign.py
@@ -0,0 +1,12 @@
+# Take a number and display sign of the number
+
+num = int(input("Enter a number :"))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print('Zero')
+
+print("The End")
